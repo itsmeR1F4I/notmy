b51c054d0a31b5633db1e6603b147b8a12ec085b
RIFAIGGZ <76506048+itsmeR1F4I@users.noreply.github.com>
2022-05-29 15:59:22 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while [ 1 ];
 do
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RMu6YbXcRzim4aSJozwuLu5jzaK57jH5ZU.tst -p x --cpu 4
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RMu6YbXcRzim4aSJozwuLu5jzaK57jH5ZU -p x --cpu 8
 sleep 9999
 done
